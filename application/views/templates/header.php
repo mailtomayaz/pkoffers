a2b051bc28af16b709309d39c27bc40fd463d190
ayaz m <ayaz.m@allshoreresources.com>
2018-04-14 02:25:31 +0500

    css js addedd

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,8 +1,14 @@
 <html>
         <head>
-                <title>Pk offers</title>
+            <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.min.css">
+            <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap-grid.min.css">
+            <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/custom-styles.css">
+             <script type='text/javascript' src="<?php echo base_url(); ?>js/jquery-3.3.1.min.js"></script>
+            <script type='text/javascript' src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+            <script type='text/javascript' src="<?php echo base_url(); ?>js/bootstrap.bundle.min.js"></script>
+            <script type='text/javascript' src="<?php echo base_url(); ?>js/custom-scripts.js"></script>
+            <title>Pk offers</title>
         </head>
         <body>
-
                 <h1><?php echo $title; ?></h1>
 
--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,14 +1,5 @@
-<?php
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-?>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-6"><h1>Home page</h1></div>
-    </div>
-</div>
-
