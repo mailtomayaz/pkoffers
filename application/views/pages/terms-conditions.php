3d96ec3cbf26b39c40479490bc1096f3bbac1a0b
ayaz m <ayaz.m@allshoreresources.com>
2018-09-25 03:18:54 +0500

    cms pages

---
+++ application/views/pages/terms-conditions.php
@@ -0,0 +1,91 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+ <section id="about" class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <div class="heading">
+            <h3><span>About us</span></h3>
+          </div>
+          <div class="sub-heading">
+            <p>
+              Creating a visual language around the beliefs of the brands we work with.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="team-box">
+            <img src="assets/img/dummies/team1.jpg" alt="" class="img-responsive" />
+            <div class="roles">
+              <h5><strong>Baby Stewards Jr</strong></h5>
+              <p>
+                CEO - Founder
+              </p>
+              <ul class="social-profile">
+                <li><a href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-dribbble fa-lg"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="team-box">
+            <img src="assets/img/dummies/team2.jpg" alt="" class="img-responsive" />
+            <div class="roles">
+              <h5><strong>Tommy Kreunichev</strong></h5>
+              <p>
+                Lead designer
+              </p>
+              <ul class="social-profile">
+                <li><a href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-dribbble fa-lg"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="team-box">
+            <img src="assets/img/dummies/team3.jpg" alt="" class="img-responsive" />
+            <div class="roles">
+              <h5><strong>Moriella Maccini</strong></h5>
+              <p>
+                Customer support
+              </p>
+              <ul class="social-profile">
+                <li><a href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-dribbble fa-lg"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="team-box">
+            <img src="assets/img/dummies/team4.jpg" alt="" class="img-responsive" />
+            <div class="roles">
+              <h5><strong>Brian James Scoothie</strong></h5>
+              <p>
+                Coffee maker
+              </p>
+              <ul class="social-profile">
+                <li><a href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-dribbble fa-lg"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
