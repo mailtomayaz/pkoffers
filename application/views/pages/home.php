0fe3405956b53a7849c64b2bc885674a56b83085
ayaz m <ayaz.m@allshoreresources.com>
2018-11-03 02:05:34 +0500

    front view modified

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -16,7 +16,8 @@
           </ul>
       </div>
       </div>
-    <ul id="slippry-slider">
+      <div class="banner-home"> </div>
+   <!--  <ul id="slippry-slider">
       <li>
         <a href="#slide1"><img src="<?php echo base_url(); ?>/img/slide/1.jpg" alt=""></a>
       </li>
@@ -26,16 +27,16 @@
       <li>
         <a href="#slide3"><img src="<?php echo base_url(); ?>/img/slide/3.jpg" alt=""></a>
       </li>
-    </ul>
+    </ul> -->
   </section>
    
-<!--      section works 
+  
   <section id="works" class="section gray">
     <div class="container">
       <div class="row">
         <div class="col-md-8 col-md-offset-2">
           <div class="heading">
-            <h3><span>Recent works</span></h3>
+            <h3><span>Our Services</span></h3>
           </div>
           <div class="sub-heading">
             <p>
@@ -44,32 +45,29 @@
           </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-12">
-          <ul class="grid effect" id="grid">
-            <li>
-              <a class="fancybox" data-fancybox-group="gallery" title="Portfolio name" href="assets/img/portfolio/1.jpg">
-								<img src="assets/img/portfolio/1.jpg" alt="" />
-							</a>
-            </li>
-            <li><a href="img/portfolio/2.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/2.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/3.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/3.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/4.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/4.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/5.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/5.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/6.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/6.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/7.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/7.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/8.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/8.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/9.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/9.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/10.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/10.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/11.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/11.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/12.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/12.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/13.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/13.jpg" alt="" /></a></li>
-            <li><a href="assets/img/portfolio/14.jpg" class="fancybox" data-fancybox-group="gallery" title="Portfolio name"><img src="assets/img/portfolio/14.jpg" alt="" /></a></li>
-          </ul>
+        <div class="row">
+<div class="box col-lg-4 col-md-4 col-sm-4 col-xs-12 ">
+<h2>Offers</h2>
+<div class="img-offers"><img class="img-responsive" src="<?php echo base_url(); ?>img/offers.jpg"></div>
+<div class="offer-descrption">here is offer desc</div>
+
+</div>
+<div class="box col-lg-4 col-md-4 col-sm-4 col-xs-12 ">
+<h2>Investment</h2>
+<div class="img-offers"><img class="img-responsive" src="<?php echo base_url(); ?>img/invenstment.jpg"></div>
+<div class="offer-descrption">here is Investment desc</div>
+
+</div>
+<div class="box col-lg-4 col-md-4 col-sm-4 col-xs-12 ">
+<h2>Charity</h2>
+<div class="img-offers"></div>
+<div class="offer-descrption">here is Charity desc</div>
+
+</div>
+
         </div>
+      </div>
+     
       </div>
     </div>
   </section>
-   section works 
- 
-   end section contact -->
